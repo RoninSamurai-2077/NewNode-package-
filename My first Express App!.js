9f436d80aef3161827ede4015630fae43f7fbe7e
Greg <Gdixon5125@gmail.com>
2021-07-01 17:37:20 -0400

    umm

--- My first Express App!.js
+++ My first Express App!.js
@@ -1,23 +1,28 @@
+let xhr = new XMLHttpRequest()
+xhr.onload
+
+xhr.open('GET', '')
+xhr.send
 //Express
 const express = require('express')
 const app = express()
- 
+var lesson = require('https://www.youtube.com/playlist?list=PLu-5lDQZVB2NWMtxP403-MwzzTcQE6F-p')
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('playlist')
 })
  
 app.listen(3000)
 //Cors
-var express = require('express')
-var cors = require('cors')
-var app = express()
+// var express = require('express')
+// var cors = require('cors')
+// var app = express()
  
-app.use(cors())
+// app.use(cors())
  
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
+// app.get('/products/:id', function (req, res, next) {
+//   res.json({msg: 'This is CORS-enabled for all origins!'})
+// })
  
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
+// app.listen(80, function () {
+//   console.log('CORS-enabled web server listening on port 80')
+// })
