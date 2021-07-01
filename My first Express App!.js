92d9602ca60c1f1b6e29120d603afb5c8a43792c
Greg <Gdixon5125@gmail.com>
2021-07-01 16:56:26 -0400

    cdc 12 server

---
+++ My first Express App!.js
@@ -0,0 +1,23 @@
+//Express
+const express = require('express')
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(3000)
+//Cors
+var express = require('express')
+var cors = require('cors')
+var app = express()
+ 
+app.use(cors())
+ 
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+ 
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
